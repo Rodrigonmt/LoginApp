2df673d3dc9de1130d4aaba1cae8b5088fa9ee17
Rodrigo Antunes <rodrigo372.nmt@gmail.com>
2025-06-24 16:58:33 -0300

    ok

---
+++ LoginApp/Controllers/ChamadoController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LoginApp.Models;
+using System.Text.Json;
+using System.Text;
+
+namespace LoginApp.Controllers
+{
+    public class ChamadoController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string firebaseUrl = "https://SEU-PROJETO.firebaseio.com/Chamados.json"; // Substitua pela sua URL Firebase
+
+        public ChamadoController()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Criar(Chamado chamado)
+        {
+            chamado.DataHoraCriacao = DateTime.Now;
+            chamado.Usuario = TempData["UsuarioLogado"]?.ToString() ?? "Desconhecido";
+
+            var json = JsonSerializer.Serialize(chamado);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync(firebaseUrl, content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                TempData["Msg"] = "Chamado criado com sucesso!";
+                return RedirectToAction("Criar");
+            }
+
+            ViewBag.Erro = "Erro ao salvar no Firebase.";
+            return View(chamado);
+        }
+    }
+}
---
+++ LoginApp/Models/Chamado.cs
@@ -0,0 +1,11 @@
+﻿namespace LoginApp.Models
+{
+    public class Chamado
+    {
+        public string Usuario { get; set; }
+        public string Equipamento { get; set; }
+        public DateTime DataHoraAgendada { get; set; }
+        public DateTime DataHoraCriacao { get; set; }
+        public string Endereco { get; set; }
+    }
+}
